1d7c3b05974887883130f75b84d51b342c68f7a0
Moritz André Myrseth <moritzmyrz@gmail.com>
2026-03-05 23:59:22 +0100

    Initial Commit

---
+++ pine/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pine
+//
+//  Created by Moritz André Myrseth on 2026-03-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pine/pineApp.swift
@@ -0,0 +1,17 @@
+//
+//  pineApp.swift
+//  pine
+//
+//  Created by Moritz André Myrseth on 2026-03-05.
+//
+
+import SwiftUI
+
+@main
+struct pineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
